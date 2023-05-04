47c7fa5d13af6fda247f417b2e936dcbd5d78668
Opie Taylor <taborn.nequil@gmail.com>
2023-05-04 15:25:00 -0500

    .replace()

--- GLab/303.3.1/src/main/java/org/example/JavaStringMethods.java
+++ GLab/303.3.1/src/main/java/org/example/JavaStringMethods.java
@@ -8,16 +8,29 @@ public class JavaStringMethods {
     public static void main(String[] args) {
 
 
-        String str1 = "Learn Java";
-        Boolean result;
-
-        result = str1.contains("Java");
-        System.out.println(result);
-
-        result = str1.contains("Python");
-        System.out.println(result);
 
 
+//String str1 = " abc cba";
+//
+//        System.out.println(str1.replace('a','z'));
+//
+//        System.out.println("Lava".replace('L','J'));
+//
+//        System.out.println("Hello".replace('4','J'));
+//
+//        System.out.println(str1.replace("C++", "Java"));
+//
+//        System.out.println("aa bb aa zz".replace("aa", "zz"));
+//
+//        System.out.println("Java".replace("C++", "C"));
+//        String str1 = "Learn Java";
+//        Boolean result;
+//
+//        result = str1.contains("Java");
+//        System.out.println(result);
+//
+//        result = str1.contains("Python");
+//        System.out.println(result);
 
 //String str1 = "Java is fun";
 //int result;
