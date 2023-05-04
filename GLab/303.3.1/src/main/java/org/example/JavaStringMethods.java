8f05c22067db064ed3637ce0ee071764bb92b7a7
Opie Taylor <taborn.nequil@gmail.com>
2023-05-04 12:42:09 -0500

    first

---
+++ GLab/303.3.1/src/main/java/org/example/JavaStringMethods.java
@@ -0,0 +1,18 @@
+package org.example;
+
+public class JavaStringMethods {
+    private static final double PI = 3.14159;
+
+    public static void main(String[] args) {
+
+     String str1 = "Java";
+     String str2 = "";
+
+        System.out.println(str1.length());
+        System.out.println(str2.length());
+        System.out.println("Java".length());
+        System.out.println("Java\n".length());
+        System.out.println("Learn Java".length());
+    }
+
+}
