c1a924a3379da443ff0771e4890f8940da2940ce
Opie Taylor <taborn.nequil@gmail.com>
2023-05-04 15:06:46 -0500

    .contains

--- GLab/303.3.1/src/main/java/org/example/JavaStringMethods.java
+++ GLab/303.3.1/src/main/java/org/example/JavaStringMethods.java
@@ -8,9 +8,26 @@ public class JavaStringMethods {
     public static void main(String[] args) {
 
 
+        String str1 = "Learn Java";
+        Boolean result;
+
+        result = str1.contains("Java");
+        System.out.println(result);
+
+        result = str1.contains("Python");
+        System.out.println(result);
 
 
 
+//String str1 = "Java is fun";
+//int result;
+//
+//result = str1.indexOf('s');
+//
+//        System.out.println(result);
+//
+//        result = str1.lastIndexOf('J');
+//        System.out.println(result);
 
 //        String str1 = "java is fun";
 //
