c139260b6f7854558fd3869a9795fed78504c7bb
Opie Taylor <taborn.nequil@gmail.com>
2023-05-05 13:10:15 -0500

    reading input from terminal

---
+++ GLab/303.3.2/src/main/java/org/example/ReadingSTringFromConsole.java
@@ -0,0 +1,25 @@
+package org.example;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ReadingSTringFromConsole {
+    private static final double PI = 3.14159;
+
+    public static void main(String[] args) {
+
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("enter three words separated by spaces: ");
+
+        String s1 = input.next();
+        String s2 = input.next();
+        String s3 = input.next();
+        System.out.println("s1 is " + s1);
+        System.out.println("s2 is " + s2);
+        System.out.println("s3 is " + s3);
+
+
+    }
+
+}
