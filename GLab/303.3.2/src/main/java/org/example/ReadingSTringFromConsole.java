39187bd6cff731ba820918c87705f96648a34a57
Opie Taylor <taborn.nequil@gmail.com>
2023-05-05 13:16:27 -0500

    first character

--- GLab/303.3.2/src/main/java/org/example/ReadingSTringFromConsole.java
+++ GLab/303.3.2/src/main/java/org/example/ReadingSTringFromConsole.java
@@ -9,15 +9,22 @@ public class ReadingSTringFromConsole {
     public static void main(String[] args) {
 
 
-        Scanner input = new Scanner(System.in);
-        System.out.println("enter three words separated by spaces: ");
+//        Scanner input = new Scanner(System.in);
+//        System.out.println("enter three words separated by spaces: ");
+//
+//        String s1 = input.next();
+//        String s2 = input.next();
+//        String s3 = input.next();
+//        System.out.println("s1 is " + s1);
+//        System.out.println("s2 is " + s2);
+//        System.out.println("s3 is " + s3);
 
-        String s1 = input.next();
-        String s2 = input.next();
-        String s3 = input.next();
-        System.out.println("s1 is " + s1);
-        System.out.println("s2 is " + s2);
-        System.out.println("s3 is " + s3);
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a character: ");
+        String s = input.nextLine();
+        char ch = s.charAt(0);
+        System.out.println("The character entered is " + ch);
+
 
 
     }
