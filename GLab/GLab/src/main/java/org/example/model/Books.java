ef72287807ef447d0ea2a0df0daed30e14b49bed
Opie Taylor <taborn.nequil@gmail.com>
2023-06-17 14:42:29 -0500

    model class for books to be added to DB

---
+++ GLab/GLab/src/main/java/org/example/model/Books.java
@@ -0,0 +1,50 @@
+package org.example.model;
+
+public class Books {
+
+    private int id;
+    private int isbn;
+    private String bookName;
+
+
+    public Books(){
+    }
+
+    public Books(int isbn, String bookName){
+        this.isbn = isbn;
+        this.bookName = bookName;
+    }
+
+    public Books(int id, int isbn,String bookName){
+        this.isbn = isbn;
+        this.bookName = bookName;
+        this.id = id;
+    }
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(int isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getBookName() {
+        return bookName;
+    }
+
+    public void setBookName(String bookName) {
+        this.bookName = bookName;
+    }
+}
+
+
+
+
