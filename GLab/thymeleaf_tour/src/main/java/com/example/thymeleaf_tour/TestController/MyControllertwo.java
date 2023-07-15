f0bc3aa93ff5b8d1162a89300e9db17c6b35852c
Opie Taylor <taborn.nequil@gmail.com>
2023-07-15 11:12:58 -0500

    @GetMapping

--- GLab/thymeleaf_tour/src/main/java/com/example/thymeleaf_tour/TestController/MyControllertwo.java
+++ GLab/thymeleaf_tour/src/main/java/com/example/thymeleaf_tour/TestController/MyControllertwo.java
@@ -1,4 +1,13 @@
 package com.example.thymeleaf_tour.TestController;
 
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class MyControllertwo {
+    @GetMapping({"login"})
+    public String showIndex(){
+        return "index";
+    }
 }
